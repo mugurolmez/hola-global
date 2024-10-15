58ad7145bd8e8cb59e899fa96d6cb840dd390444
mugurolmez <m.ugurolmez@hotmail.com>
2024-10-16 01:31:04 +0300

    cors config

--- holaglobal/src/main/java/com/holadev/HolaGlobal/security/CorsConfig.java
+++ holaglobal/src/main/java/com/holadev/HolaGlobal/security/CorsConfig.java
@@ -1,6 +1,7 @@
 package com.holadev.HolaGlobal.security;
 
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,7 +13,10 @@ public class CorsConfig {
 
     @Value("${frontend.url}")
     private String frontendUrl;
-
+    @PostConstruct
+    public void printFrontendUrl() {
+        System.out.println("Frontend URL: " + frontendUrl);
+    }
     @Bean
     public WebMvcConfigurer webMvcConfigurer() {
         return new WebMvcConfigurer() {
